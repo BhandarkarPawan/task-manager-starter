ae5efb61e25dfbfcf324f2ddb5f5cdd10d55d955
BhandarkarPawan <pawankbhandarkar@gmail.com>
2023-11-15 19:51:52 -0800

    Starter Code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <p1>Hello World!</p1>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </div>
     );
 }
