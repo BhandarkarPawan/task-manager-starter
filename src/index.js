42ff41ef718e4040d37dbc845fcf34a63aa17f75
BhandarkarPawan <pawankbhandarkar@gmail.com>
2023-11-15 23:28:15 -0800

    Add injection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <StyledEngineProvider injectFirst>
+            <App />
+        </StyledEngineProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
