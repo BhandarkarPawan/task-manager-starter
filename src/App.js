0d1cb99ee8ceb5466813b078af8d9f66cd7c8b4e
BhandarkarPawan <pawankbhandarkar@gmail.com>
2023-11-16 01:34:32 -0800

    sadsad

--- src/App.js
+++ src/App.js
@@ -13,14 +13,6 @@ import TodoList from "./components/TodoList";
 // 5. Create a Modal component for adding a new task
 // 6. Persist and read state from local storage
 
-// Tasks to complete:
-// 1. Create a Header component using Material UI (AppBar)
-// 2. Create a Todo Item component and customize styling using Tailwind
-// 3. Create two lists: Pending and Completed Tasks with Static Props
-// 4. Make the lists work with dynamic props & prop drilling
-// 5. Create a Modal component for adding a new task
-// 6. Persist and read state from local storage
-
 function App() {
     const [items, setItems] = useState(localStorage.getItem("items") || []);
 
