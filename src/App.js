5e8ba526bf27ee7c3349ef9213c1a5cf64eb6512
BhandarkarPawan <pawankbhandarkar@gmail.com>
2023-11-16 01:32:17 -0800

    update

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ import Header from "./components/Header";
 // 5. Create a Modal component for adding a new task
 // 6. Persist and read state from local storage
 
-// Tasks to complete:
-// 1. Create a Header component using Material UI (AppBar)
-// 2. Create a Todo Item component and customize styling using Tailwind
-// 3. Create two lists: Pending and Completed Tasks with Static Props
-// 4. Make the lists work with dynamic props & prop drilling
-// 5. Create a Modal component for adding a new task
-// 6. Persist and read state from local storage
-
 function App() {
     return (
         <div className="App">
